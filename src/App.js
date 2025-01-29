8868976b6815dd02e841b57870114c1e06fd853a
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2025-01-29 16:26:55 +0530

    Implementing redux functionalities

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Counter from './components/Count';
+
+function App() {
+  return (
+    <div className="App">
+      <button>Increment</button>
+      <Counter/>
+      <button>Decrement</button>
+    </div>
+  );
+}
+
+export default App;
